1c49749a8d78a753845b6a3fb14d1659b509db37
Jared Hislop <jared.hislop@challengervault.com>
2018-10-04 13:36:25 -0600

    updated registration

--- registration.php
+++ registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Tealium_Tags',
+    'Tealium_TemoTags',
     __DIR__
 );
