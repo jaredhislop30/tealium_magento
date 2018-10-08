6a5c330e12902aba2d4b3149d3c63b456ca92e19
Jared Hislop <jared.hislop@challengervault.com>
2018-10-08 10:17:30 -0600

    removed temp

--- registration.php
+++ registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Tealium_TempTags',
+    'Tealium_Tags',
     __DIR__
 );
