3cb5771b994da2442011d2e1c832f09f4ed35ae0
Jared Hislop <jared.hislop@challengervault.com>
2018-10-11 13:07:22 -0600

    test

--- Observer/Observer.php
+++ Observer/Observer.php
@@ -7,24 +7,24 @@ use Magento\Framework\Event\ObserverInterface;
 class Observer implements ObserverInterface
 {
     public function __construct() {
- 
+        print("add to cart");
         $product = Mage::getModel('catalog/product')
                         ->load(Mage::app()->getRequest()->getParam('product', 0));
  
         if (!$product->getId()) {
             return;
         }
-        print("add to cart");
+        
         $categories = $product->getCategoryIds();
  
-        Mage::getModel('core/session')->setProductToShoppingCart(
-            new Varien_Object(array(
-                'id' => $product->getId(),
-                'qty' => Mage::app()->getRequest()->getParam('qty', 1),
-                'name' => $product->getName(),
-                'price' => $product->getPrice(),
-                'category_name' => Mage::getModel('catalog/category')->load($categories[0])->getName(),
-            ))
-        );
+        // Mage::getModel('core/session')->setProductToShoppingCart(
+        //     new Varien_Object(array(
+        //         'id' => $product->getId(),
+        //         'qty' => Mage::app()->getRequest()->getParam('qty', 1),
+        //         'name' => $product->getName(),
+        //         'price' => $product->getPrice(),
+        //         'category_name' => Mage::getModel('catalog/category')->load($categories[0])->getName(),
+        //     ))
+        // );
     }
 }
