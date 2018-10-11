0e7de8347745b61577f60c43f879c5a6a6ec8e69
Jared Hislop <jared.hislop@challengervault.com>
2018-10-11 10:37:51 -0600

    test

--- Block/Tealium.php
+++ Block/Tealium.php
@@ -212,7 +212,7 @@ EOD;
         // Render Tealium tag in javaScript
         $insert_tag = <<<EOD
 (function(a,b,c,d){
-    a='//tags.tiqcdn.com/utag/$this->account/$this->profile/$this->target/utag.js$this->cache_bust';
+    a='//tags.tiqcdn.com/utag/$this->account/$this->profile/$this->target/utag.js';
     b=document;c='script';d=b.createElement(c);d.src=a;d.type='text/java'+c; 
     d.async=true;
     a=b.getElementsByTagName(c)[0];a.parentNode.insertBefore(d,a);
