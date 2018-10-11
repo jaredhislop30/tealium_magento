3afe156c00482ac6d6bff0817447ab0657b18533
Jared Hislop <jared.hislop@challengervault.com>
2018-10-11 12:52:15 -0600

    product add to cart

---
+++ Observer/Observer.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tealium\Tags\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+
+class Observer implements ObserverInterface
+{
+    public function __construct() {
+ 
+        $product = Mage::getModel('catalog/product')
+                        ->load(Mage::app()->getRequest()->getParam('product', 0));
+ 
+        if (!$product->getId()) {
+            return;
+        }
+        print("add to cart");
+        $categories = $product->getCategoryIds();
+ 
+        Mage::getModel('core/session')->setProductToShoppingCart(
+            new Varien_Object(array(
+                'id' => $product->getId(),
+                'qty' => Mage::app()->getRequest()->getParam('qty', 1),
+                'name' => $product->getName(),
+                'price' => $product->getPrice(),
+                'category_name' => Mage::getModel('catalog/category')->load($categories[0])->getName(),
+            ))
+        );
+    }
+}
