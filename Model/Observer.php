d22171bc51885cda109d80cbc2e42cd11bb376f0
Jared Hislop <jared.hislop@challengervault.com>
2018-10-12 08:47:21 -0600

    test

--- Model/Observer.php
+++ Model/Observer.php
@@ -5,7 +5,8 @@ class Tealium_Tags_Model_Observer {
         //$observer contains data passed from when the event was triggered.
         //You can use this data to manipulate the order data before it's saved.
         //Uncomment the line below to log what is contained here:
-        //Mage::log($observer);
+        print("this is a test");
+        Mage::log($observer);
 
         Mage::log('We just made an Observer!');
     }
