befc29b5b57f15641ac51bae08ff1f13ec79e443
Jared Hislop <jared.hislop@challengervault.com>
2018-10-23 16:25:14 -0600

    udpated observer.php

--- Observer/Observer.php
+++ Observer/Observer.php
@@ -13,47 +13,23 @@ use Magento\Framework\Event\ObserverInterface;
 class Observer implements ObserverInterface
 {
 
-    /**
-     * @var ObjectManagerInterface
-     */
-    protected $_objectManager;
-
-    /**
-     * @var \Magento\Framework\App\Request\Http
-     */
-    protected $_request;
-
-    /**
-     * @param \Magento\Framework\ObjectManagerInterface $objectManager
-     */
     public function __construct(
         \Magento\Framework\ObjectManagerInterface $objectManager,
         \Magento\Framework\App\Request\Http $request
     ) {
-        $this->_objectManager = $objectManager;
-        $this->_request = $request;
+        print("Construct Add to Cart");
     }
 
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
-        $product = Mage::getModel('catalog/product')
-                        ->load(Mage::app()->getRequest()->getParam('product', 0));
+        $product = $observer->getProduct();
+
+        $originalName = $product->getName();
  
-        if (!$product->getId()) {
-            return;
-        }
+        $modifiedName = $originalName . ' - Modified by Magento 2 Events and Observers - execute';
  
-        $categories = $product->getCategoryIds();
- 
-        Mage::getModel('core/session')->setProductToShoppingCart(
-            new Varien_Object(array(
-                'id' => $product->getId(),
-                'qty' => Mage::app()->getRequest()->getParam('qty', 1),
-                'name' => $product->getName(),
-                'price' => $product->getPrice(),
-                'category_name' => Mage::getModel('catalog/category')->load($categories[0])->getName(),
-            ))
-        );
+        $product->setName($modifiedName);
+        print("Execute add to cart");
     }
 
 }
