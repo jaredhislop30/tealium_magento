fae2f5187f5c101baeb0bd06a1849b1f10b0ace3
Jared Hislop <jared.hislop@challengervault.com>
2018-12-06 10:33:13 -0700

    test product URL

--- Helper/Product.php
+++ Helper/Product.php
@@ -33,6 +33,12 @@ class Product extends AbstractHelper
     public function getProductData($product_id, $array = true) {
         $result = [];
         $product = $this->_productRepository->getById($product_id);
+        
+        print($product->getImage());
+        print($product->getData('image'));
+        print($product->getData('thumbnail'));
+        print($product->getData('small_image'));
+
         $result['product_name'] = [(string)$product->getName()];
         $result['product_list_price'] = [(string)number_format((float)$product->getPrice(), 2, '.', '')];
         $result['product_sku'] = [(string)$product->getSku()];
