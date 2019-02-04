6aa5adacf7859707d4b6c85fe08a3b7cd2b3e17c
Jared Hislop <jared.hislop30@gmail.com>
2019-02-04 09:21:02 -0700

    updated user logout event

--- view/frontend/web/js/userLogout.js
+++ view/frontend/web/js/userLogout.js
@@ -7,7 +7,7 @@ define([
         if (document.querySelectorAll('body > div.page-wrapper > header > div.panel.wrapper > div > ul > li.customer-welcome > div > ul > li.authorization-link > a').length != 0) {
             $.get(location.protocol + '//' + location.host+'/tealium/cart/user', function(dataJson) {
             	$($('body > div.page-wrapper > header > div.panel.wrapper > div > ul > li.customer-welcome > div > ul > li.authorization-link > a')[0]).on('click', function() {
-                    var data = JSON.parse(dataJson);
+                    var data = dataJson;
                     if (data['id'][0]) {
                         var tealiumTag = window.utag;
                         data['tealium_event'] = ['user_logout'];
